0ef3464cf03b57b42a2bc6dc4086e7dc59cafc67
Kunal Gupta <ioskunal@gmail.com>
2020-02-16 18:31:11 +0530

    Initial Commit

---
+++ SlideToPayButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideToPayButton
+//
+//  Created by Kunal Gupta on 16/02/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
